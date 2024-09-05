1a15db226447768befc5b10608330e64bde38357
DanieleChecchin <danielechecchin26@gmail.com>
2024-09-05 14:32:05 +0200

    MILESTONE 1 e 2

--- script.js
+++ script.js
@@ -3,7 +3,22 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            toDoList: [
+                { text: 'Fare la spesa', done: false },
+                { text: 'Pagare le bollette', done: false },
+                { text: 'Controllare le mail', done: true },
+                { text: 'Fare la lavatrice', done: false },
+                { text: 'Lavare il pavimento', done: true },
+                { text: 'Guardare la partita', done: false },
+                { text: 'Andare al bar', done: true },
+                { text: 'Mettere la sveglia', done: true }
+            ]
+        }
+    },
+    methods: {
+        deleteTask(index) {
+            this.toDoList.splice(index, 1)
         }
     }
 }).mount('#app')
+
